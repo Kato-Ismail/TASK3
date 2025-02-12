47f3bbed2b9f465425a4157c6ba58e0db29af44c
Kato-Ismail <Katoismail9999@gmail.com>
2025-02-12 16:59:11 +0300

    commit 2

---
+++ src/ContactManagementApp.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+
+public class ContactManagementApp {
+    private JFrame frame;
+    private JPanel cardPanel;
+    private CardLayout cardLayout;
+
+    private ArrayList<Contact> contacts;
+    private JList<Contact> contactList;
+    private DefaultListModel<Contact> listModel;
+
+}
